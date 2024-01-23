9ebf0903c9557f5eb47875fbcc588c2bc1ac5e68
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-23 16:22:05 +0300

    add DocumentHandler application runner

---
+++ DocumentHandler/src/main/java/com/andpostman/documenthandler/DocumentHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.andpostman.documenthandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentHandlerApplication.class, args);
+    }
+
+}
