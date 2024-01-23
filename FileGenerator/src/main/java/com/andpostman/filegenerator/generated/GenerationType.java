19ae30d7505b8060ce197bfbeaa0f40f105235ec
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-23 16:30:46 +0300

    add FileGenerator generation xml types

---
+++ FileGenerator/src/main/java/com/andpostman/filegenerator/generated/GenerationType.java
@@ -0,0 +1,21 @@
+package com.andpostman.filegenerator.generated;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum GenerationType {
+
+    VALID_ONE(1),
+    VALID_THREE(3),
+    VALID_CAPACITY(25_000),
+    XSD_ERROR(1),
+    FORM_TYPE_ERROR(1);
+
+    private int count;
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
