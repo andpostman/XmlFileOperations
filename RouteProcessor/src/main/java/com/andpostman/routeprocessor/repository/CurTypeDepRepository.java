83785276f33dbe7f9bb3504daaaf4f2be2d6b58d
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-23 16:54:00 +0300

    add RouteProcessor db repository for CurTypeDep

---
+++ RouteProcessor/src/main/java/com/andpostman/routeprocessor/repository/CurTypeDepRepository.java
@@ -0,0 +1,7 @@
+package com.andpostman.routeprocessor.repository;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import com.andpostman.routeprocessor.model.CurTypeDep;
+
+public interface CurTypeDepRepository extends ReactiveCrudRepository<CurTypeDep, Integer> {
+}
