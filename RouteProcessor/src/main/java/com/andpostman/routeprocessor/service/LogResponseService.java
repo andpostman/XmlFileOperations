ff0526b15e447fe5638a0d450154a67917b4e818
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-23 17:00:23 +0300

    add RouteProcessor business logic for logger

---
+++ RouteProcessor/src/main/java/com/andpostman/routeprocessor/service/LogResponseService.java
@@ -0,0 +1,7 @@
+package com.andpostman.routeprocessor.service;
+
+import com.andpostman.routeprocessor.property.LogResponse;
+
+public interface LogResponseService {
+    void printLogResponse(LogResponse response);
+}
