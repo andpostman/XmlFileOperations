488b3680f3591e839889fd538e7834b8f3c09c09
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-23 17:09:43 +0300

    add RouteProcessor default test

---
+++ RouteProcessor/src/test/java/com/andpostman/routeprocessor/RouteProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andpostman.routeprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
