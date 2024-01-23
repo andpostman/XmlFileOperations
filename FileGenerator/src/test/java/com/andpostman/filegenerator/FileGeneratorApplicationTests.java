332c3fcb48352ac69e724a34511b0ac07a4580cf
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-23 16:37:10 +0300

    add FileGenerator default test

---
+++ FileGenerator/src/test/java/com/andpostman/filegenerator/FileGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andpostman.filegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
