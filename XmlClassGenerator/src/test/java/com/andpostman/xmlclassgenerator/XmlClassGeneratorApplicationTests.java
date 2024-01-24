2eb3fc62d9c11185f29d526d2c1c5cc487337b0e
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-24 17:00:21 +0300

    add XmlClassGenerator default test

---
+++ XmlClassGenerator/src/test/java/com/andpostman/xmlclassgenerator/XmlClassGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andpostman.xmlclassgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlClassGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
